a997bfb04260258f3bf225f28dcb2175ddb53887
Jose Antonio Perez Gonzalez <114741595+Jpergon251@users.noreply.github.com>
2026-08-06 18:37:55 +0200

    Cambiado el aviso para que se vea en moviles tambien

--- src/components/screens/WelcomeScreen.vue
+++ src/components/screens/WelcomeScreen.vue
@@ -30,9 +30,32 @@
             </template>
 
 
-            <template v-else>
+<template v-else>
 
-                <div class="browser-warning">
+    <div class="browser-warning">
+
+        <template v-if="isMobile">
+
+                    <h3>
+                        Dispositivo no compatible
+                    </h3>
+
+                    <p>
+                        Calliope actualmente está disponible únicamente para ordenadores.
+                    </p>
+
+                    <p>
+                        Los navegadores móviles no permiten acceder a carpetas locales de música de forma segura.
+                    </p>
+
+                    <p class="unsupported-note">
+                        Puedes acceder desde un PC usando un navegador basado en <strong>Chromium</strong>.
+                    </p>
+
+                </template>
+
+
+                <template v-else>
 
                     <h3>
                         Navegador no compatible
@@ -43,47 +66,55 @@
                     </p>
 
                     <p>
-                        Puedes usar cualquiera de estos navegadores:
+                        Puedes utilizar cualquiera de estos navegadores:
                     </p>
 
 
                     <div class="browser-grid">
 
                         <div class="browser-card">
+
                             <img 
                                 src="https://images.icon-icons.com/836/PNG/512/Google_Chrome_icon-icons.com_66794.png"
                                 alt="Chrome"
                             >
+
                             <a 
                                 href="https://www.google.com/intl/es_es/chrome/"
                                 target="_blank"
-                                rel="noopener noreferrer"    
+                                rel="noopener noreferrer"
                             >
                                 Chrome
                             </a>
+
                         </div>
 
 
                         <div class="browser-card">
+
                             <img 
                                 src="https://upload.wikimedia.org/wikipedia/commons/7/7e/Microsoft_Edge_logo_%282019%29.png"
                                 alt="Edge"
                             >
+
                             <a 
                                 href="https://explore.microsoft.com/es-es/edge/download?form=MA13FJ"
                                 target="_blank"
-                                rel="noopener noreferrer"    
+                                rel="noopener noreferrer"
                             >
                                 Edge
                             </a>
+
                         </div>
 
 
                         <div class="browser-card">
+
                             <img 
                                 src="https://img.icons8.com/color/512/brave-web-browser.png"
                                 alt="Brave"
                             >
+
                             <a 
                                 href="https://brave.com/es/download/"
                                 target="_blank"
@@ -91,6 +122,7 @@
                             >
                                 Brave
                             </a>
+
                         </div>
 
                     </div>
@@ -105,10 +137,13 @@
                         Firefox y Safari todavía no soportan esta función.
                     </p>
 
-                </div>
+                </template>
 
-            </template>
+            </div>
 
+        </template>
+
+            
         </div>
 
     </section>
@@ -116,17 +151,18 @@
 
 <script setup>
 
+import { computed } from "vue";
 import Logo from "../common/Logo.vue";
 import { useLibraryStore } from "../../stores/libraryStore";
-import { computed } from "vue";
-
 
 const library = useLibraryStore();
 
-
-const isSupported = computed(() => {
-    return "showDirectoryPicker" in window;
+const isMobile = computed(() => {
+  return /Android|iPhone|iPad|iPod/i.test(navigator.userAgent);
 });
 
+const isSupported = computed(() => {
+  return !isMobile.value && "showDirectoryPicker" in window;
+});
 
 </script>
